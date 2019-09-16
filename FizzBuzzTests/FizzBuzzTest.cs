73e2f2ef27e0431287ed9f718f2d0cd6975a7c08
TakaK22 <25346710+TakaK22@users.noreply.github.com>
2019-09-16 10:22:27 +0900

    テストをパラメータテストに書き換え

--- FizzBuzzTests/FizzBuzzTest.cs
+++ FizzBuzzTests/FizzBuzzTest.cs
@@ -6,18 +6,20 @@ namespace FizzBuzzTests
     [TestClass]
     public class FizzBuzzTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow("1", 1)]
+        [DataRow("2", 2)]
+        public void TestMethod1(string expected, int number)
         {
-            string actual = FizzBuzz1.Convert(1);
-            string expected = "1";
+            string actual = FizzBuzz1.Convert(number);
             Assert.AreEqual(expected, actual);
         }
-        [TestMethod]
-        public void TestMethod2()
+
+        [DataTestMethod]
+        [DataRow("Fizz", 3)]
+        public void TestMethod2(string expected, int number)
         {
-            string actual = FizzBuzz1.Convert(3);
-            string expected = "Fizz";
+            string actual = FizzBuzz1.Convert(number);
             Assert.AreEqual(expected, actual);
         }
     }
