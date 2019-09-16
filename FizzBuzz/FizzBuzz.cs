85df8ec23bf66cdc666f7ed8b7444b92b875f5ef
TakaK22 <25346710+TakaK22@users.noreply.github.com>
2019-09-16 10:54:15 +0900

    5の倍数の時Buzzを返す処理を実装

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -7,6 +7,7 @@ namespace FizzBuzz
         public static string Convert(int number)
         {
             if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
             return number.ToString();
         }
     }
